dde6cc28006c2b35bf3062727044c2fd53627310
keanuplayz <keanutimmermans8@gmail.com>
2020-05-21 19:55:16 +0200

    Refactored menu code to global function.

--- commands/menu.js
+++ commands/menu.js
@@ -1,48 +1,7 @@
 /* eslint-disable no-unused-vars */
 const Discord = require("discord.js");
 exports.run = async (client, message, args, level) => {
-    const pages = ["Pages?", "Page two?", "Is this page three?"];
-    let page = 1;
-    const embed = new Discord.RichEmbed()
-        .setColor(0xffffff)
-        .setFooter(`Page ${page} of ${pages.length}.`)
-        .setDescription(pages[page - 1]);
-
-    message.channel.send(embed).then(msg => {
-        msg.react("⬅").then(r => {
-            msg.react("➡");
-
-            const backwardsFilter = (reaction, user) => reaction.emoji.name == "⬅" && user.id === message.author.id;
-            const forwardsFilter = (reaction, user) => reaction.emoji.name == "➡" && user.id === message.author.id;
-
-            const backwards = msg.createReactionCollector(backwardsFilter, {
-                time: 60000
-            });
-            const forwards = msg.createReactionCollector(forwardsFilter, {
-                time: 60000
-            });
-
-            backwards.on("collect", r => {
-                if (page === 1) return;
-                page--;
-                msg.reactions.find(reaction => reaction.emoji.name == "⬅")
-                    .remove(message.author);
-                embed.setDescription(pages[page - 1]);
-                embed.setFooter(`Page ${page} of ${pages.length}.`);
-                msg.edit(embed);
-            });
-
-            forwards.on("collect", r => {
-                if (page === pages.length) return;
-                page++;
-                msg.reactions.find(reaction => reaction.emoji.name == "➡")
-                    .remove(message.author);
-                embed.setDescription(pages[page - 1]);
-                embed.setFooter(`Page ${page} of ${pages.length}.`);
-                msg.edit(embed);
-            });
-        });
-    });
+    client.pages(message, ["Page 1", "Page 2"]);
 };
 
 exports.conf = {
--- modules/functions.js
+++ modules/functions.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js");
 module.exports = (client) => {
 
   client.permlevel = message => {
@@ -26,6 +27,50 @@ module.exports = (client) => {
     "welcomeMessage": "Say hello to {{user}}, everyone! We all need a warm welcome sometimes :D",
     "welcomeEnabled": "false"
   };
+
+  client.pages = (message, pages) => {
+    let page = 1;
+    const embed = new Discord.RichEmbed()
+        .setColor(0xffffff)
+        .setFooter(`Page ${page} of ${pages.length}.`)
+        .setDescription(pages[page - 1]);
+
+    message.channel.send(embed).then(msg => {
+        msg.react("⬅").then(r => {
+            msg.react("➡");
+
+            const backwardsFilter = (reaction, user) => reaction.emoji.name == "⬅" && user.id === message.author.id;
+            const forwardsFilter = (reaction, user) => reaction.emoji.name == "➡" && user.id === message.author.id;
+
+            const backwards = msg.createReactionCollector(backwardsFilter, {
+                time: 60000
+            });
+            const forwards = msg.createReactionCollector(forwardsFilter, {
+                time: 60000
+            });
+
+            backwards.on("collect", r => {
+                if (page === 1) return;
+                page--;
+                msg.reactions.find(reaction => reaction.emoji.name == "⬅")
+                    .remove(message.author);
+                embed.setDescription(pages[page - 1]);
+                embed.setFooter(`Page ${page} of ${pages.length}.`);
+                msg.edit(embed);
+            });
+
+            forwards.on("collect", r => {
+                if (page === pages.length) return;
+                page++;
+                msg.reactions.find(reaction => reaction.emoji.name == "➡")
+                    .remove(message.author);
+                embed.setDescription(pages[page - 1]);
+                embed.setFooter(`Page ${page} of ${pages.length}.`);
+                msg.edit(embed);
+            });
+        });
+    });
+  }
 
   client.getSettings = (guild) => {
     client.settings.ensure("default", defaultSettings);
