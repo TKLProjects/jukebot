7f39ce35418c9f1c5f0c91de972bd4b4723b0e52
TravBot <hondzee69@gmail.com>
2020-05-25 19:30:42 +0000

    Travis beautify build 9

--- commands/songs.js
+++ commands/songs.js
@@ -5,12 +5,12 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args, level) => {
     const directoryPath = path.join(__dirname, 'storage');
     console.log(directoryPath);
-    fs.readdir(directoryPath, function (err, files) {
+    fs.readdir(directoryPath, function(err, files) {
         if (err) {
             return console.log("Unable to scan directory:" + err);
         }
 
-        files.forEach(function (file) {
+        files.forEach(function(file) {
             console.log(file);
         });
     });
