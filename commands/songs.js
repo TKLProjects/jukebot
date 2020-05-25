4b62fe4b011fbcecdcdc612e876b01ec36717549
Keanu <keanutimmermans8@gmail.com>
2020-05-25 19:28:54 +0000

    Added simple songs command.
    Currently only lists files in /commands/storage.

---
+++ commands/songs.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+const path = require("path");
+const fs = require("fs");
+const Discord = require("discord.js");
+exports.run = async (client, message, args, level) => {
+    const directoryPath = path.join(__dirname, 'storage');
+    console.log(directoryPath);
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            return console.log("Unable to scan directory:" + err);
+        }
+
+        files.forEach(function (file) {
+            console.log(file);
+        });
+    });
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "songs",
+    category: "Utility",
+    description: "Lists songs.",
+    usage: "songs"
+};
